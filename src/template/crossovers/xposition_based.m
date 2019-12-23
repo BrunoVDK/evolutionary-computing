27fe79e6f58d3fd6236dc1fd9377d810d58d42e7
Bruno Vandekerkhove <brunovandekerkhove@protonmail.com>
2019-12-23 21:38:24 +0100

    Position-Based (POS)

--- src/template/crossovers/xposition_based.m
+++ src/template/crossovers/xposition_based.m
@@ -5,31 +5,17 @@ function NewChromosome = xposition_based(OldChromosome, CrossoverRate)
     NewChromosome = OldChromosome; % Initialise new chromosome matrix
     for row = 1:2:(rows-rem(rows,2)) % Mate parents
         if rand < CrossoverRate % Recombine with a given probability
-            positions = randperm(randi(cols));
-            NewChromosome(row,:) = position(OldChromosome(row,:), OldChromosome(row+1,:), left, right);
-            NewChromosome(row+1,:) = position(OldChromosome(row+1,:), OldChromosome(row,:), left, right);
+            positions = randperm(randi(cols))
+            NewChromosome(row,:) = position(OldChromosome(row,:), OldChromosome(row+1,:), positions);
+            NewChromosome(row+1,:) = position(OldChromosome(row+1,:), OldChromosome(row,:), positions);
         end
     end
     
     % Create offspring
-    function offspring = position(first, second, left, right)
-        
-        offspring = first;
-        idx = right;
-        for i = right+1:cols
-            idx = idx + 1 - cols * (idx == cols);
-            while ismember(second(idx),first(left:right))
-                idx = idx + 1 - cols * (idx == cols);
-            end
-            offspring(i) = second(idx);
-        end
-        for i = 1:left-1
-            idx = idx + 1 - cols * (idx == cols);
-            while ismember(second(idx),first(left:right))
-                idx = idx + 1 - cols * (idx == cols);
-            end
-            offspring(i) = second(idx);
-        end
+    function offspring = position(first, second, positions)
+        offspring = second;
+        mask = second(positions);
+        offspring(setdiff(1:cols,positions)) = first(not(ismember(first,mask)));
     end
    
 end
