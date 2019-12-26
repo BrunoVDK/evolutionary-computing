b65b353c894120a24770896e544f57d8c62a4dd3
Bruno Vandekerkhove <brunovandekerkhove@protonmail.com>
2019-12-26 19:53:56 +0100

    Update confidence_intervals.m

--- src/template/experiments/confidence_intervals.m
+++ src/template/experiments/confidence_intervals.m
@@ -1,6 +1,5 @@
-function [interval] = confidence_intervals(x)
+function [interval] = confidence_interval(x)
     standard_error = std(x) / sqrt(length(x));
     ts = tinv([0.025  0.975], length(x) - 1); % Inverse student t
     interval = mean(x) + ts * standard_error %#ok
 end
-
