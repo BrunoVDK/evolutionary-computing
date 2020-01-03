cba640afda33b82a4f55a920bf6b9789b9dac814
Unknown <michael.dooreman@gmail.com>
2020-01-03 11:36:38 +0100

    Added longest streak std variant

--- src/template/experiments/longest_streak.m
+++ src/template/experiments/longest_streak.m
@@ -17,5 +17,26 @@ function [longest] = longest_streak(best_list)
             i = i + 1;
         end
         longest = best_sofar;
+end
+    
+function [longest] = longest_streak2(best_list, delta)
+        current_value = []; % since best -1 does not exist
+        current_counter = 0;
+        best_sofar = 0;
+        i = 1;
+        while i <= size(best_list,2)
+            t = best_list(1,i);
+            if (size(current_value) > 0) && (t/current_value(1)) >= delta
+                if current_counter > best_sofar
+                    best_sofar = current_counter;
+                end
+                current_counter = 1;
+            else
+                current_counter = current_counter + 1;
+            end
+            current_value(size(current_value)+1) = t;
+            i = i + 1;
+        end
+        longest = best_sofar;
     end
 
